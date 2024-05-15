8bf6c6d609cc70d76688368085abe4556573a2f0
zmzeng12 <zmzeng12@foxmail.com>
2024-05-15 21:08:34 +0800

    add app

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, jsonify, request
+
+from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType, InteractionResponseFlags
+
+CLIENT_PUBLIC_KEY = os.getenv('CLIENT_PUBLIC_KEY')
+
+app = Flask(__name__)
+
+@app.route('/interactions', methods=['POST'])
+@verify_key_decorator(CLIENT_PUBLIC_KEY)
+def interactions():
+    if request.json['type'] == InteractionType.APPLICATION_COMMAND:
+        return jsonify({
+            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            'data': {
+                'content': 'Hello world'
+            }
+        })
+    elif request.json['type'] == InteractionType.MESSAGE_COMPONENT:
+        return jsonify({
+            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            'data': {
+                'content': 'Hello, you interacted with a component.',
+                'flags': InteractionResponseFlags.EPHEMERAL
+            }
+        })
+        
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
---
+++ start.sh
@@ -0,0 +1,5 @@
+set -ex
+
+pip3 install discord-interactions flask
+
+python3 app.py
